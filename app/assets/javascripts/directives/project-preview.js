7393b948c0a54a0068c39d7ae11da5d8c6318192
Grant Schulte <schulte.grant@gmail.com>
2015-07-26 18:35:22 -0400

    stylings

---
+++ app/assets/javascripts/directives/project-preview.js
@@ -0,0 +1,33 @@
+portfolioApp.directive('projectCard', [function() {
+  return {
+    restrict: 'A',
+    link: function(scope, el) {
+      var projectBox = $(el);
+      var imageBox   = projectBox.find('.image-box');
+      var previewBox = projectBox.find('.preview');
+
+      var config = {
+        animLength: 400,
+        animEasing: 'easeOutQuint'
+      }
+
+      projectBox.hover(function() {
+        previewBox.animate({
+          'top': '0'
+        }, config.animLength, config.animEasing);
+
+        imageBox.animate({
+          'top': '100%'
+        }, config.animLength, config.animEasing);
+      }, function() {
+        previewBox.animate({
+          'top': '-100%'
+        }, config.animLength, config.animEasing);
+
+        imageBox.animate({
+          'top': '0'
+        }, config.animLength, config.animEasing);
+      });
+    }
+  };
+}]);

522c3d086a1b54cf50762ebadd06d9b871a0c970
Grant Schulte <schulte.grant@gmail.com>
2016-02-10 22:43:55 -0500

    adds auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  USERS = { "grant" => "mememe" }
+
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_digest("Application") do |name|
+      USERS["grant"]
+    end
+  end
 end
